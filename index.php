805d2726769d819bd69739764febeb1eb80df3cd
Andre2903 <128695158+Andre2903@users.noreply.github.com>
2023-03-25 13:15:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,22 @@
-<?php
-
-for($i=1;$i<=9;$i++){
-    for($j=1;$j>=0;$j--){
-        echo  "";
-
-    }
-    for($k=$i;$k<=9;$k++){
-        echo $k;
-    }
-    echo "<br>";
-}
-
-?>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Andre Gunawan | D112121051</title>
+  </head>
+  <body>
+    <h1> Tugas Quiz 2</h1>
+    <?php
+    $jumlah =9;
+    for($i=1; $i<=$jumlah; $i++) {
+        $penampung ='' ;
+        for($z=$i; $z<=$jumlah; $z++){
+            $penampung .=$z;
+        }
+        echo $penampung. "<br>"; 
+        } 
+        echo 0; 
+        ?>
+  </body>
+</html>
